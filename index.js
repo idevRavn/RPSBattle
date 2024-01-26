f9e9f0ec60171bb57be941c4a8cfe5983f87022f
Sanu Kumar <idevravn@gmail.com>
2024-01-26 21:06:51 +0530

    Update index.js
    Added each round function and the game function in which the html elements are changed after 5 rounds, specifying the winner

--- index.js
+++ index.js
@@ -24,3 +24,54 @@ function getPlayerChoice() {
     return "error";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "tie";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "win";
+  } else {
+    return "lose";
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let round = 1;
+  for (round; round <= 5; round++) {
+    let result = playRound(getPlayerChoice(), getComputerChoice());
+
+    if (result === "win") {
+      playerScore++;
+    } else if (result === "lose") {
+      computerScore++;
+    } else {
+      playerScore++;
+      computerScore++;
+    }
+  }
+  document.querySelector(
+    "#playerScore"
+  ).textContent = `Player Score: ${playerScore}`;
+  document.querySelector(
+    "#computerScore"
+  ).textContent = `Computer Score: ${computerScore}`;
+  if (playerScore > computerScore) {
+    console.log("You won");
+    document.querySelector("#gameWinner").textContent = "You won!";
+  } else if (playerScore < computerScore) {
+    document.querySelector("#gameWinner").textContent =
+      "Computer win the game!";
+    console.log("Computer won");
+  } else {
+    console.log("It's a tie");
+    document.querySelector("#gameWinner").textContent = "It's a tie!";
+  }
+}
+
+game();
