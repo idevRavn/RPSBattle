3102abb5b2a0882f473f58df70231901afb567f8
Sanu Kumar <idevravn@gmail.com>
2024-01-26 20:51:11 +0530

    Update index.js
    Added computer choice function

---
+++ index.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      return "error";
+  }
+}
